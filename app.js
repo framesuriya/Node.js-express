40f2795c83f74d614525aab05cc9726da9c022fe
Suriya Takong <takong.suriya@gmail.com>
2023-02-27 22:56:25 +0700

    rm nmd

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const debug = require('debug')('app');
 const chalk = require('chalk');
 const morgan = require('morgan')
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
 const path = require('path');
+app.set("views", "./src/views")
+app.set("view engine", "ejs")
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")))
 app.get("/", (req, res) => {
-    res.send('Hi Hii Hiiii');
+    res.render('index', { username: 'Framesuriya', customers: ['Frame1', 'Frame2', 'Frame3'] });
 })
-
-app.listen(port, () => {
-    console.log('Listening on port ' + chalk.green(port));
+app.listen(PORT, () => {
+    console.log('Listening on PORT ' + chalk.green(PORT));
 })
